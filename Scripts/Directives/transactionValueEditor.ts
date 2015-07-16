d63b4493409cb16ae9644552311032d37d5816af
burtality69 <jamesrmccallum@gmail.com>
2015-07-15 19:39:01 -0700

    tveditor updated

--- Scripts/Directives/transactionValueEditor.ts
+++ Scripts/Directives/transactionValueEditor.ts
@@ -5,23 +5,71 @@ module Budgeter.Controllers {
 	
 	export class transactionValueEditorCtrl {
 		
-		static $inject = ['transactionValueMgr','notify','$rootScope','listOptionsDataSvc'];
+		static $inject = ['transactionValueMgr','cgNotify','$rootScope','listOptionsDataSvc'];
 		listOptions: Budgeter.Services.listOptionsDataSvc;
 		tv: ITransactionValueModel; 
-		transactionValueMgr: Budgeter.Services.transactionValueMgr
+		transactionValueMgr: Budgeter.Services.transactionValueMgr;
+		frequencies: Array<string>;
+		notify: ng.cgNotify.INotifyService;
+		newitem: boolean;
+		liststate: ITransValueListState;
 		
 		constructor($scope: Budgeter.Controllers.ITransactionValueListScope){
 			
 			if ($scope.liststate.tvToEdit != undefined) {
 				this.tv = $scope.liststate.tvToEdit;
+				this.newitem = true; 
 			} else {
 				this.tv = this.transactionValueMgr.getnewTransactionValue();
+				this.newitem = false; 
 			}
-				
+			
+			this.getfrequencies();		
 		}
 		
-		getfrequencies(){
-			this
+		getfrequencies() {
+			this.listOptions.transactionfrequencies
+				.success(d => {
+					this.frequencies = d;
+				})
+				.error(e => {
+					this.notify({ message: 'There was a problem loading data', classes: 'alert-danger' })
+				})
+		}
+		
+		/** either post or put a transactionvalue depending on values in liststate */
+		submit() {
+			if (this.newitem) {
+				this.transactionValueMgr.post(this.tv)
+					.success(d=> {
+						this.notify({ message: 'Item created successfully', classes: 'alert-success' })
+					})
+					.error(e=> {
+						this.notify({ message: 'There was a problem submitting the item', classes: 'alert-danger' })
+					});
+			} else {
+				this.transactionValueMgr.put(this.tv)
+					.success(d=> {
+						this.notify({ message: 'Item created successfully', classes: 'alert-success' })
+					})
+					.error(e=> {
+						this.notify({ message: 'There was a problem submitting the item', classes: 'alert-danger' })
+					});
+			}
+		}
+		
+		cancel() {
+			this.liststate.addEdit = false;
+		}
+		
+		delete() {
+			this.transactionValueMgr.delete(this.tv.ID)
+				.success(d=> {
+					this.notify({message: 'Item deleted successfully',classes: 'alert-success'})
+				})
+				.error(e=> {
+					this.notify({message: 'Error' + e ,classes: 'alert-danger'})
+				})
 		}
 	}
 }
